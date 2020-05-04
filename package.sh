ccd1f91572f5ee86dabd025a1defcc727f44cf2c
Alon Carmel <alon.carmel@cyberark.com>
2020-05-04 22:33:56 +0300

    pull phusion base image

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11.4.12-c959595'
-
+docker pull registry.tld/ruby-fips-base-image-phusion:1.0.0
 docker run --rm $DEBIFY_IMAGE config script > docker-debify
 chmod +x docker-debify
 
